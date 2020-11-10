32b319e1c994647b2a452805ff51a07e5a8202fa
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2020-11-10 22:21:47 +0900

    deploy

--- config.rb
+++ config.rb
@@ -44,3 +44,46 @@ page '/*.txt', layout: false
 #   activate :minify_css
 #   activate :minify_javascript
 # end
+
+activate :deploy do |deploy|
+  deploy.method = :rsync
+  deploy.host          = 'www.example.com'
+  deploy.path          = '/srv/www/site'
+  # Optional Settings
+  # deploy.user  = 'tvaughan' # no default
+  # deploy.port  = 5309 # ssh port, default: 22
+  # deploy.clean = true # remove orphaned files on remote host, default: false
+  # deploy.flags = '-rltgoDvzO --no-p --del' # add custom flags, default: -avz
+end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
+activate :deploy do |deploy|
+  deploy.method   = :ftp
+  deploy.host            = 'ftp.example.com'
+  deploy.path            = '/srv/www/site'
+  deploy.user            = 'tvaughan'
+  deploy.password        = 'secret'
+end
+
+activate :deploy do |deploy|
+  deploy.method   = :sftp
+  deploy.host            = 'sftp.example.com'
+  deploy.port            = 22
+  deploy.path            = '/srv/www/site'
+  # Optional Settings
+  # deploy.user     = 'tvaughan' # no default
+  # deploy.password = 'secret' # no default
+end
+
+activate :deploy do |deploy|
+  # ...
+  deploy.build_before = true # default: false
+end
