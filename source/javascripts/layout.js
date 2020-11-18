4acef886bf33f1556d68f848a4de87aede873fb2
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2020-11-18 13:01:26 +0900

    add animation

--- source/javascripts/layout.js
+++ source/javascripts/layout.js
@@ -6,3 +6,17 @@ $(function() {
     $(this).removeClass('js-over');
   })
 });
+
+$(function() {
+  $(window).scroll(function() {
+    let position = $(window).scrollTop();
+    console.log(position);
+    if (position > 120) {
+      console.log("aaaa");
+      $(".text.js-before").fadeIn("slow");
+    }
+    if (position > 300) {
+      $(".text.js-after").fadeIn("slow");
+    }
+  }) 
+});
