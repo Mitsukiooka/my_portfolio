de826b5e236fc83601a7594ef2d5319862df5e66
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2021-05-14 15:45:57 +0900

    minor upgrade

--- source/javascripts/layout.js
+++ source/javascripts/layout.js
@@ -1,9 +1,21 @@
-$(function() {
-  $(".btn").mouseover(function() {
+$(function(){
+  $('.btn').on('mouseover', function(){
     $(this).addClass('js-over');
   })
-  $(".btn").mouseout(function() {
+  $('.btn').on('mouseout', function(){
     $(this).removeClass('js-over');
+  })
+});
+
+$(function(){
+  $(window).scroll(function(){
+    let position = $(window).scrollTop();
+    if (position > 120) {
+      console.log(position)
+      $('.text').animate({
+      }, 300
+      );
+    }
   })
 });
 
@@ -12,7 +24,6 @@ $(function() {
 //     let position = $(window).scrollTop();
 //     console.log(position);
 //     if (position > 120) {
-//       console.log("aaaa");
 //       $(".text.js-before").fadeIn("slow");
 //     }
 //     if (position > 300) {
